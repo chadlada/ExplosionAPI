17f953369c44532b9db882b50355de306ceae387
Chad <chadlada@gmail.com>
2022-01-30 19:41:38 -0500

    Added Explosion(works),Mumbling(needtofinish) controllers

--- Controllers/ExplosionController.cs
+++ Controllers/ExplosionController.cs
@@ -11,5 +11,27 @@ namespace ExplosionAPI.Controllers
     [ApiController]
     public class ExplosionController : ControllerBase
     {
+        [HttpGet("{str}")]
+        public string Explosion(string str = "1234")  //Why isn't it defaulting when I run in insomnia?
+        {
+            // var newString = "";
+            // foreach (var number in str)
+            // {
+            //     for (var index = 0; index < number - '0'; index++)
+            //     {
+            //         newString += number;
+            //     }
+            // }
+            // return newString;
+            return string.Concat(str.Select(n => new string(n, int.Parse(n.ToString()))));
+        }
     }
 }
+
+
+// * Create a controller for explosion
+//     * Create a new Controller called ExplosionController
+//     * This will have one HttpGet method
+//     * The method will accept a string either as a URL parameter or a query parameter. Your choice.
+//     * This endpoint method will take the string passed to it, manipulate it according to the kata Digits Explosion and return the result
+//     * Have your code return the string
